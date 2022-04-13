4b2058b020b384ed73d07ad57a6b427d9b8eb706
Littlemanman <1830234303@qq.com>
2022-04-14 03:11:10 +0800

    add early-init file, init startup config

---
+++ early-init.el
@@ -0,0 +1,14 @@
+;;; package --- summary
+;;; Commentary:
+;;; Code:
+(setq package-enable-at-startup nil)
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+;; 禁止启动屏幕
+(setq inhibit-startup-message t)
+
+(provide 'early-init)
+;;; early-init.el ends here
