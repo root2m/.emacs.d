aafee1185f5725fd63381ec72978bd0a866e9577
Littlemanman <1830234303@qq.com>
2022-04-14 20:10:56 +0800

    byte compile org config file

--- init.el
+++ init.el
@@ -10,11 +10,37 @@
   (setenv "PATH" "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl")
   (setq exec-path (split-string "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl" path-separator)))
 
+(defvar lmmv/emacs-config-file "EmacsConfig.org"
+  "Main Config file, .org or .el.")
 
-(org-babel-load-file
- (expand-file-name
-  "EmacsConfig.org"
-  user-emacs-directory))
+(defvar lmmv/emacs-config-tangle-file (concat (file-name-base lmmv/emacs-config-file) ".el")
+  "Config file for 'lmmv/emacs-config-file', if it is a org file.")
+
+(defun lmm/emacs-load-config()
+  "Load config file, '.el' or '.org' file."
+  (let* ((init-file (expand-file-name  lmmv/emacs-config-tangle-file user-emacs-directory))
+         (init-org-file (expand-file-name lmmv/emacs-config-file user-emacs-directory)))
+    (if (file-exists-p init-file)
+        (load-file init-file)
+      (if (file-exists-p init-org-file)
+          (org-babel-load-file init-org-file)
+        (display-warning :warning (format "File %s not exitsed!!!" init-org-file))))))
+
+(lmm/emacs-load-config)
+
+(defun lmm/emacs-build-config-file()
+  "Build config file for org, add to 'kill-emacs-hook'."
+  (interactive)
+  (let* ((init-file (expand-file-name  lmmv/emacs-config-tangle-file user-emacs-directory))
+         (init-org-file (expand-file-name lmmv/emacs-config-file user-emacs-directory)))
+    (when (file-exists-p init-file)
+      (delete-file init-file))
+    (when (file-exists-p init-org-file)
+      (org-babel-tangle-file init-org-file init-file)
+      (let ((byte-compile-warnings nil))
+        (byte-compile-file init-file)))))
+
+(add-hook 'kill-emacs-hook #'lmm/emacs-build-config-file)
 
 ;; 让hook顺序倒过来， 符合EmacsConfig.org配置编写逻辑
 (setq after-init-hook (reverse after-init-hook))
