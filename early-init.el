fec38d55a0e25f658207c546789242cb16ae7f52
Littlemanman <1830234303@qq.com>
2022-04-19 20:18:27 +0800

    add gc

--- early-init.el
+++ early-init.el
@@ -6,6 +6,10 @@
 ;; use straight package manager.
 (setq package-enable-at-startup nil)
 
+(setq garbage-collection-messages t)
+(setq gc-cons-threshold (* 100 1024 1024))
+(setq gc-cons-percentage 0.6)
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
