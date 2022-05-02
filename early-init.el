fcfec1d9449e53048495c85135454916e8f34f34
Littlemanman <1830234303@qq.com>
2022-05-03 01:31:09 +0800

    add tree-sitter-object color-rg config key

--- early-init.el
+++ early-init.el
@@ -9,6 +9,10 @@
 (setq garbage-collection-messages t)
 (setq gc-cons-threshold (* 100 1024 1024))
 (setq gc-cons-percentage 0.6)
+(setq default-frame-alist '((width . 200)
+                            (height . 50)
+                            (left . 100)
+                            (top . 50)))
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
