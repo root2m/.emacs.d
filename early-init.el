4022e2f966c424052e7d32d867bd812ecf1e54e2
root2m <lmm@RAZEN-2600>
2022-12-05 21:50:32 +0800

    move to early-init.el

--- early-init.el
+++ early-init.el
@@ -3,16 +3,21 @@
 ;;; Code:
 
 
-;; use straight package manager.
-(setq package-enable-at-startup nil)
+(setq garbage-collection-messages t
+      gc-cons-threshold (* 100 1024 1024)
+      gc-cons-percentage 0.6
 
-(setq garbage-collection-messages t)
-(setq gc-cons-threshold (* 100 1024 1024))
-(setq gc-cons-percentage 0.6)
-(setq default-frame-alist '((width . 200)
+      default-frame-alist '((width . 200)
                             (height . 50)
                             (left . 100)
-                            (top . 50)))
+                            (top . 50))
+
+      pgtk-wait-for-event-timeout 0.001
+
+      ;; Increase how much is read from processes in a single chunk (default is 4kb)
+      read-process-output-max (* 4 1024 1024)
+
+      package-enable-at-startup nil)
 
 (menu-bar-mode -1)
 (tool-bar-mode -1)
