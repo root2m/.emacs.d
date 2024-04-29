7fee399c0bcc61add3d6708421fa814c9d4307c2
root2m <lmm@RAZEN-2600.>
2024-04-29 19:21:10 +0800

    no littering!!!

--- early-init.el
+++ early-init.el
@@ -19,6 +19,11 @@
 
       package-enable-at-startup nil)
 
+(when (fboundp 'startup-redirect-eln-cache)
+  (startup-redirect-eln-cache
+   (convert-standard-filename
+    (expand-file-name  "var/eln-cache/" user-emacs-directory))))
+
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
