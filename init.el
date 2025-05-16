456f45fa1b5378fda48fe5e8d64299d91ccc4af1
root2m <lmm@RAZEN-2600.localdomain>
2025-05-17 03:00:26 +0800

    delete wsl check. delete emacs-nox check.

--- init.el
+++ init.el
@@ -4,23 +4,6 @@
 ;;; Code:
 
 
-(defvar lmmv/emacs-in-wsl (file-exists-p "/mnt/wsl")
-  "Emacs if run wsl, is t else nil.")
-
-(defvar lmmv/emacs-nox-p (string-match "--with-x=no" system-configuration-options nil t)
-  "Emacs if no x, is t else nil.")
-
-(when lmmv/emacs-in-wsl
-  (let ((path (replace-regexp-in-string "/mnt[^:]*:?" "" (getenv "PATH"))))
-    (setenv "PATH" path)
-    (setq exec-path (split-string path ":"))
-    (setq browse-url-browser-function (lambda(url &rest args)
-                                        (start-process
-                                         (concat "WinExplorer " url)
-                                         nil
-                                         "/mnt/c/Windows/explorer.exe"
-                                         url)))))
-
 (defvar lmmv/emacs-config-file "EmacsConfig.org"
   "Main Config file, .org or .el.")
 
@@ -31,16 +14,7 @@
   "Load config file, '.el' or '.org' file."
   (interactive "P")
   (let* ((init-file (expand-file-name  lmmv/emacs-config-tangle-file user-emacs-directory))
-         (init-org-file (expand-file-name lmmv/emacs-config-file user-emacs-directory))
-         (fun (make-symbol "lmm/org-babel-get-src-block-info-change")))
-    (when lmmv/emacs-nox-p
-      (fset fun (lambda (info)
-                  (when info
-                    (if (assq :nox (nth 2 info))
-                        (setf (cdr (assq :tangle (nth 2 info))) init-file)
-                      (setf (cdr (assq :tangle (nth 2 info))) "no")))
-                  info))
-      (advice-add 'org-babel-get-src-block-info :filter-return fun))
+         (init-org-file (expand-file-name lmmv/emacs-config-file user-emacs-directory)))
     (cond ((and (numberp load) (> load 4))
            (org-babel-load-file init-org-file))
           (load
@@ -49,9 +23,7 @@
              (org-babel-load-file init-org-file)))
           (t
            (require 'org)
-           (org-babel-tangle-file init-org-file init-file)))
-    (when lmmv/emacs-nox-p
-      (advice-remove 'org-babel-get-src-block-info fun))))
+           (org-babel-tangle-file init-org-file init-file)))))
 
 (lmm/emacs-build-config-file 'load)
 
